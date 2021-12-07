4072ef7207163c23ffb363a172e7ad6f12e7999a
Nandana Raj <nandana_raj1@baylor.edu>
2021-12-06 18:46:51 -0600

    commit

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,74 @@
-from django.shortcuts import render
+from django.forms.fields import DateTimeField
+from django.shortcuts import redirect, render
+from .forms import EntryForm, TopicForm
+from .models import Topic, Entry
 
 # Create your views here.
 def index(request):
     #get: requesting information from the database
     #post: positing information to the database
     return render(request, 'MainApp/index.html')
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    #'-' before date added means ascending
+
+    context = {'topics':topics}
+
+    return render(request, 'MainApp/topics.html', context)
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+
+    entries = topic.entry_set.all()
+
+    context = {'topic':topic, 'entries':entries}
+
+    return render(request, 'MainApp/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('MainApp:Topics')
+
+    context = {'form':form}
+    return render(request, 'MainApp/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('MainApp:Topics',topic_id=topic_id)
+
+    context = {'form':form, 'topic':topic}
+    return render(request, 'MainApp/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    #edit an existing entry
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        #This arguments tells Django to create the form prefilled with information from the existing entry object
+        form = EntryForm(instance=entry)
+    else:
+        #POST data submitted; process data
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('MainApp:topic', topic_id=topic.id)
+
+    context = {'entry':entry, 'topic':topic, 'form':form}
+    return render(request, 'MainApp/edit_entry.html', context)
